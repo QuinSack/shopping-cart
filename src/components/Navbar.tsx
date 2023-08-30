98975cf2f7871b81455c282853a2ea08d2aca6b4
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-08-30 23:01:57 +0000

    built navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Navbar as NavbarBs, Container, Nav, NavLink, Button} from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+  return (
+    <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
+        <Container>
+            <Nav className='me-auto'>
+                <Nav.Link href='/' as={NavLink}>
+                    Home
+                </Nav.Link>
+                <Nav.Link href='/store' as={NavLink}>
+                    Store
+                </Nav.Link>
+                <Nav.Link href='/about' as={NavLink}>
+                    About
+                </Nav.Link>
+            </Nav>
+            <Button style={{width: '3rem', height: '3rem', position: 'relative'}} variant='outline-primary' className='rounded-circle'>
+                <FontAwesomeIcon icon={faCartPlus} size='2x'></FontAwesomeIcon>
+                <div className='rounded-circle bg-danger d-flex justify-content-center align-items-center' style={{color:'white', width: '1.5rem', height: '1.5rem', position: 'absolute', bottom: '0', right: '0', transform: 'translate(25%, 25%)'}}>
+                    4
+                </div>
+            </Button>
+        </Container>
+    </NavbarBs>
+  )
+}
+
+export default Navbar
