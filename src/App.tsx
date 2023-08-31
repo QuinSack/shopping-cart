47d862368edbb4d144efc3480768006d5d1a0dcc
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-08-31 20:26:49 +0000

    enabled crud operations with the store items

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import Home from './pages/Home';
 import Store from './pages/Store';
 import About from './pages/About';
 import Navbar from './components/Navbar';
+import { ShoppingCartProvider } from './contexts/ShoppingCartContext';
 
 function App() {
   return (
     <>
+    <ShoppingCartProvider>
       <Navbar />
-      <Container className='mb-4'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/store' element={<Store />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-      </Container>
+        <Container className='mb-4'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/store' element={<Store />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </Container>
+    </ShoppingCartProvider>
     </>
     
   );
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import storeItems from '../data/items.json'
+import { Col, Row } from 'react-bootstrap'
+import StoreItem from '../components/StoreItem'
 
+
+// interface Props{
+//     id: number,
+//     name: string,
+//     price: number,
+//     image: string
+//   }
 const Store = () => {
+  
   return (
-    <div>Store</div>
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className='g-3'>
+        {storeItems.map((item)=> (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
